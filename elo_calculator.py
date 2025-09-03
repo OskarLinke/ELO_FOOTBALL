7c1183f958fdec50201d4af5e9e0dd985728818f
Oskar Linke <olinke2@gmail.com>
2025-09-03 22:23:34 +0200

    did some work

---
+++ elo_calculator.py
@@ -0,0 +1,111 @@
+import numpy as np
+import pickle as pkl
+import pandas as pd
+
+with open('teams_dict.pkl', 'rb') as f:
+    teams_dict = pkl.load(f)
+
+
+
+top100_row = ['team_name', 2000, 'date']
+
+top100 = np.array([top100_row] * 100)
+
+
+games_df = pd.read_csv("all_games.csv")
+
+
+def calc_elo(home_team, away_team, result): 
+    #All calculations from https://en.wikipedia.org/wiki/Elo_rating_system
+    if result == 'H': 
+        res = 1 
+    elif result == 'D': 
+        res = 0.5
+    elif result == 'A': 
+        res = 0 
+    else: 
+        raise ValueError('Unexpected result of game (Not H, D or A)')
+    Ra = teams_dict[home_team]['elo']
+    Rb = teams_dict[away_team]['elo']
+    Qa = 10**(Ra/400)
+    Qb = 10 ** (Rb/400)
+    Ea = Qa/(Qa + Qb)
+    Eb = Qb/(Qa + Qb)
+    if teams_dict[home_team]['provisional']: 
+        Ra_ = Ra + 32 * (res - Ea)
+    else: 
+        Ra_ = Ra + 16 *  (res-Ea)
+
+    if teams_dict[away_team]['provisional']: 
+        Rb_ = Rb + 32 * ((1-res) - Eb)
+    else: 
+        Rb_ = Rb + 16 * ((1-res)-Eb)   
+    
+    
+    return Ra_ , Rb_
+
+
+def run_game(home_team, away_team, result, date, top100): 
+    teams_dict[home_team]['elo'], teams_dict[away_team]['elo'] = calc_elo(home_team, away_team, result)
+    
+
+    if (teams_dict[home_team]['elo'] > teams_dict[home_team]['max_elo']) and (not teams_dict[home_team]['provisional']): 
+        teams_dict[home_team]['max_elo'] = teams_dict[home_team]['elo']
+        teams_dict[home_team]['max_elo_date'] = date
+
+        if teams_dict[home_team]['elo'] > float(top100[0][1]):
+            if home_team in top100[:,0]: 
+                mask = top100[:,0] == home_team 
+                top100[mask,1] = teams_dict[home_team]['elo']
+                top100[mask,2] = date
+            else: 
+                top100[0] = [home_team, teams_dict[home_team]['elo'], date]
+            top100 = top100[top100[:,1].argsort()]
+
+
+    if (teams_dict[away_team]['elo'] > teams_dict[away_team]['max_elo']) and (not teams_dict[away_team]['provisional']): 
+        teams_dict[away_team]['max_elo'] = teams_dict[away_team]['elo']
+        teams_dict[away_team]['max_elo_date'] = date
+
+        if teams_dict[away_team]['elo'] > float(top100[0][1]):
+            if away_team in top100[:,0]: 
+                mask = top100[:,0] == away_team 
+                top100[mask,1] = teams_dict[away_team]['elo']
+                top100[mask,2] = date
+            else: 
+                top100[0] = [away_team, teams_dict[away_team]['elo'], date]
+            top100 = top100[top100[:,1].argsort()]
+
+            
+    teams_dict[home_team]['games_played'] += 1
+    teams_dict[away_team]['games_played'] += 1 
+
+    if teams_dict[home_team]['games_played'] >= 100: 
+        teams_dict[home_team]['provisional'] = False 
+    
+    if teams_dict[away_team]['games_played'] >= 100: 
+        teams_dict[away_team]['provisional'] = False 
+    
+    return top100
+
+
+
+
+for row in games_df.itertuples():
+
+    top100 = run_game(row[2], row[3], row[4], row[1], top100)
+
+
+
+
+
+with open('finished_teams_dict.pkl', 'wb') as f: 
+    pkl.dump(teams_dict, f)
+
+with open('finished_top100.pkl', 'wb') as f: 
+    pkl.dump(top100, f)
+
+print(top100)
+
+
+
--- teams_dict.py
+++ teams_dict.py
@@ -3,7 +3,7 @@ import pickle as pkl
 
 df = pd.read_csv("all_games.csv")
 
-all_teams = df['home_team'].unique() 
+all_teams = pd.unique(df[['home_team', 'away_team']].values.ravel('K')) 
 
 #We want to sort the data by date 
 
@@ -14,32 +14,33 @@ all_teams_dict = {}
 df_leagues_only = df[df.tournament != "FA"]
 df_leagues_only = df_leagues_only[df_leagues_only.tournament != "LC"]
 
-all_teams_in_leagues = df_leagues_only['home_team'].unique()
-
-
-print(len(all_teams))
-print(len(all_teams_in_leagues))
-for team in all_teams_in_leagues: 
-    first_appearance = df_leagues_only[df_leagues_only['home_team'] == team].iloc[0]
-
-    if first_appearance['tournament'] == "PL": 
-        all_teams_dict[team] = {"elo": 2600, "provisional": True, "max_elo": 2600, "max_elo_date": first_appearance['date']}
-    elif first_appearance['tournament'] == "CS":
-        all_teams_dict[team] = {"elo": 2500, "provisional": True, "max_elo": 2500, "max_elo_date": first_appearance['date']}
-    elif first_appearance['tournament'] == "L1": 
-        all_teams_dict[team] = {"elo": 2350, "provisional": True, "max_elo": 2350, "max_elo_date": first_appearance['date']}
-    elif first_appearance['tournament'] == "L2": 
-        all_teams_dict[team] = {"elo": 2200, "provisional": True, "max_elo": 2200, "max_elo_date": first_appearance['date']}
-
-
+all_teams_in_leagues = pd.unique(df_leagues_only[['home_team', 'away_team']].values.ravel('K'))
 for team in all_teams: 
     if team in all_teams_in_leagues: 
-        continue 
-    else: 
-        first_appearance = df[df['home_team'] == team].iloc[0]
-        all_teams_dict[team] = {"elo": 2000, "provisional": True, "max_elo": 2000, "max_elo_date": first_appearance['date']}
+      
+        appearances = (df_leagues_only['home_team'] == team) | (df_leagues_only['away_team'] == team) 
+        first_row_idx = appearances.idxmax()
+        first_appearance = df_leagues_only.loc[first_row_idx]
 
-print(all_teams_dict)
+        if first_appearance['tournament'] == "PL": 
+            all_teams_dict[team] = {"elo": 2600, "provisional": True, "max_elo": 2600, "max_elo_date": first_appearance['date'], "games_played": 0}
+        elif first_appearance['tournament'] == "CS":
+            all_teams_dict[team] = {"elo": 2500, "provisional": True, "max_elo": 2500, "max_elo_date": first_appearance['date'], "games_played": 0}
+        elif first_appearance['tournament'] == "L1": 
+            all_teams_dict[team] = {"elo": 2350, "provisional": True, "max_elo": 2350, "max_elo_date": first_appearance['date'], "games_played": 0}
+        elif first_appearance['tournament'] == "L2": 
+            all_teams_dict[team] = {"elo": 2200, "provisional": True, "max_elo": 2200, "max_elo_date": first_appearance['date'], "games_played": 0}
+
+
+    else:
+        
+        appearances = (df['home_team'] == team) | (df['away_team'] == team) 
+        first_row_idx = appearances.idxmax()
+        first_appearance = df.loc[first_row_idx]
+        all_teams_dict[team] = {"elo": 2000, "provisional": True, "max_elo": 2000, "max_elo_date": first_appearance['date'], "games_played": 0}
+
+
+all_teams_dict['wolverhampton'] = all_teams_dict.pop('wolverhampton-wanderers')
 
 with open('teams_dict.pkl', 'wb') as f:
     pkl.dump(all_teams_dict, f)
